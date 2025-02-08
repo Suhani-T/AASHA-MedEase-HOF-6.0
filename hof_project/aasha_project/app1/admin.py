033c13f5ac15e75d2f801145add5999ae72dae0b
AnushkaPankhuri <anushka.pat.bih@gmail.com>
2025-02-08 21:32:12 +0530

    profiles functionality

--- hof_project/aasha_project/app1/admin.py
+++ hof_project/aasha_project/app1/admin.py
@@ -1,1 +1,15 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Doctor, Patient
+
+admin.site.register(Doctor)
+admin.site.register(Patient)
+
+
+class CustomUserAdmin(UserAdmin):  
+    list_display = ('username', 'email', 'is_doctor', 'is_patient', 'is_active', 'is_staff')  
+    list_filter = ('is_doctor', 'is_patient', 'is_active')    
+    search_fields = ('username', 'email')  
+
+admin.site.register(CustomUser, CustomUserAdmin) 
 
